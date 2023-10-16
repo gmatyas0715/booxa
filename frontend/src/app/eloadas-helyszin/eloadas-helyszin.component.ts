02f7a0fe000edd03514648874d52911fb532e65a
gmatyas0715 <gombocz.matyas@students.jedlik.eu>
2023-10-16 22:27:56 +0200

    bejelentkezett user adatok módosítása

--- frontend/src/app/eloadas-helyszin/eloadas-helyszin.component.ts
+++ frontend/src/app/eloadas-helyszin/eloadas-helyszin.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, numberAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-koncerthely-megjelenito',
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./eloadas-helyszin.component.css']
 })
 export class EloadasHelyszinComponent {
+  @ViewChild('canvas') canvasRef: ElementRef | null = null;
+  
+  ngAfterViewInit(): void {
+    const canvas: HTMLCanvasElement = this.canvasRef?.nativeElement;
+    const ctx = canvas?.getContext('2d');
 
+
+    window.addEventListener('resize',function(){
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    });
+
+    let mouse: {
+      x: number,
+      y: number,
+    } = {x:0,y:0}
+    canvas.addEventListener('click',function(event){
+      mouse.x = event.x;
+      mouse.y = event.y;
+      drawCircle();
+    });
+
+    canvas.addEventListener('mousemove',function(event){
+      mouse.x = event.x;
+      mouse.y = event.y;
+      drawCircle();
+    });
+
+    function drawCircle(){
+      if (ctx!=null){
+        ctx.fillStyle = 'black';
+        ctx.beginPath();
+        ctx.arc(mouse.x,mouse.y,50,0,Math.PI*2);
+        ctx.fill();
+      }
+    }
+  }
 }
