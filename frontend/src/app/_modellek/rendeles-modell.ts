30b49ac4595801f2610d6aa519c35c9a519cd75d
gmatyas0715 <gombocz.matyas@students.jedlik.eu>
2023-10-23 12:08:13 +0200

    rendeles-modell class változók felvitele

--- frontend/src/app/_modellek/rendeles-modell.ts
+++ frontend/src/app/_modellek/rendeles-modell.ts
@@ -1,2 +1,7 @@
 export class RendelesModell {
+    public rendelesID:number = 0;
+    public rendelesIdopont:Date = new Date();
+    public userID:number = 0;
+    public fizetesAdatID:number = 0;
+    public szallitasID:number = 0;
 }
