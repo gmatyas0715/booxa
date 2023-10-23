c9f9f62cfb4c506492aeb6b778c727a3f7767afb
gmatyas0715 <gombocz.matyas@students.jedlik.eu>
2023-10-23 12:25:24 +0200

    esemeny-modell class változók felvitele

--- frontend/src/app/_modellek/esemeny-modell.ts
+++ frontend/src/app/_modellek/esemeny-modell.ts
@@ -1,2 +1,10 @@
 export class EsemenyModell {
+    public esemenyID:number = 0;
+    public eloado:string = "";
+    public helyszin:string = "";
+    public esemenyIdopont:Date = new Date();
+    public szabadteri:boolean = true;
+    public jegyAlapar:number = 0;
+    public eloadoID:number = 0;
+    public helyszinID:number = 0;
 }
