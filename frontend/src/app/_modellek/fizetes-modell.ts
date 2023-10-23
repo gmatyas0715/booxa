361c1c6d4bd595f357eca9a67647798597a9847b
gmatyas0715 <gombocz.matyas@students.jedlik.eu>
2023-10-23 12:19:16 +0200

    fizetes-modell class változók felvitele

--- frontend/src/app/_modellek/fizetes-modell.ts
+++ frontend/src/app/_modellek/fizetes-modell.ts
@@ -1,2 +1,7 @@
 export class FizetesModell {
+    public fizetesID:number = 0;
+    public fizetesIdopont:Date = new Date();
+    public fizetesOsszeg:number = 0;
+    public fiezetesTipusa:string = "";
+    public szamlazasiCim:string = "";
 }
