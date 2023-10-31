e58d91ce52806375bea1fe00a301f2ce50a4e379
gmatyas0715 <gombocz.matyas@students.jedlik.eu>
2023-10-31 21:23:02 +0100

    minta svg készítés előadás helyszínhez

--- frontend/src/app/esemeny-reszletek/esemeny-reszletek.component.ts
+++ frontend/src/app/esemeny-reszletek/esemeny-reszletek.component.ts
@@ -1,41 +1,26 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-esemeny-reszletek',
   templateUrl: './esemeny-reszletek.component.html',
   styleUrls: ['./esemeny-reszletek.component.css']
 })
-export class EsemenyReszletekComponent {
-/*@ViewChild('canvas') canvasRef: ElementRef | null = null;
-  
-  ngAfterViewInit(): void {
-    const canvas: HTMLCanvasElement = this.canvasRef?.nativeElement;
-    const ctx = canvas?.getContext('2d');
+export class EsemenyReszletekComponent{
+  public id:string = "";
 
+  Kattintas(event:Event):void{
+      const element = event.target as HTMLElement;
+      const elementId = element.id;
+      console.log(elementId)
+  }
 
-    window.addEventListener('resize',function(){
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
-    });
+  FillValtoztatas(event:Event){
+    const element = event.target as SVGAElement;
+    element.style.fill = 'blue';
+  }
 
-    let mouse: {
-      x: number,
-      y: number,
-    } = {x:0,y:0}
-
-    canvas.addEventListener('mousemove',function(event){
-      mouse.x = event.x;
-      mouse.y = event.y;
-      drawCircle();
-    });
-
-    function drawCircle(){
-      if (ctx!=null){
-        ctx.fillStyle = 'black';
-        ctx.beginPath();
-        ctx.arc(mouse.x,mouse.y,50,0,Math.PI*2);
-        ctx.fill();
-      }
-    }
-  }*/
+  FillVisszavaltoztatas(event:Event){
+    const element = event.target as SVGAElement;
+    element.style.fill = 'white';
+  }
 }
