7aaa65766d32ad3f5483fd0826c7064e4572f8fa
gmatyas0715 <gombocz.matyas@students.jedlik.eu>
2023-11-13 18:38:23 +0100

    sql scriptek egy része hozzáadva

---
+++ backend/sql scriptek/eloado_mufaj.sql
@@ -0,0 +1,446 @@
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (1,29);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (1,39);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (2,25);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (3,31);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (4,32);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (5,29);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (5,33);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (5,34);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (5,39);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (6,26);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (6,29);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (6,31);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (6,32);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (6,47);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (6,48);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (7,38);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (7,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (7,47);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (8,31);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (8,47);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (8,26);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (8,31);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (8,48);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (8,43);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (8,49);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (9,28);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (9,47);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (9,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (10,26);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (10,31);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (10,32);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (10,47);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (10,50);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (11,31);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (11,41);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (11,47);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (11,48);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (11,26);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (12,50);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (12,48);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (12,47);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (12,41);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (12,32);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (12,31);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (12,26);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (13,18);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (13,19);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (13,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (14,18);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (14,19);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (14,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (14,51);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (15,19);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (16,19);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (17,19);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (17,18);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (17,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (18,18);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (18,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (19,18);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (19,31);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (19,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (19,48);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (20,18);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (20,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (20,48);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (20,49);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (21,18);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (21,51);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (21,31);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (21,43);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (21,52);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (22,18);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (23,29);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (23,39);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (24,29);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (24,39);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (25,29);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (25,39);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (25,41);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (26,29);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (26,39);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (26,31);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (27,35);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (27,29);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (27,41);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (28,48);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (28,35);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (28,41);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (29,35);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (29,51);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (29,41);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (30,19);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (30,35);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (31,35);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (31,50);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (31,53);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (32,35);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (32,53);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (33,35);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (33,41);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (33,54);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (34,35);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (34,54);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (35,32);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (36,32);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (37,32);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (38,32);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (39,32);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (40,18);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (40,51);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (40,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (41,17);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (41,27);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (41,28);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (42,18);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (42,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (43,37);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (43,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (44,55);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (44,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (45,28);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (45,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (45,56);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (46,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (46,28);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (47,28);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (47,18);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (48,37);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (48,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (48,56);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (49,56);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (49,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (49,37);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (50,38);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (50,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (50,17);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (51,38);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (51,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (51,17);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (52,57);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (52,51);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (52,38);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (53,38);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (53,57);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (54,17);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (54,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (55,17);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (55,27);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (55,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (56,58);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (56,17);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (56,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (57,58);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (57,59);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (57,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (58,58);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (58,51);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (58,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (59,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (59,53);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (59,58);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (60,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (60,50);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (60,38);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (60,58);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (61,17);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (61,58);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (62,17);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (62,58);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (63,59);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (63,51);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (63,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (64,59);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (64,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (65,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (65,59);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (66,25);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (66,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (66,22);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (67,60);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (67,50);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (68,50);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (68,45);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (69,50);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (69,30);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (70,50);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (70,30);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (71,50);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (71,30);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (71,21);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (72,50);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (72,30);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (72,44);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (73,50);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (73,30);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (73,44);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (74,50);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (74,30);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (74,44);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (75,50);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (75,44);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (76,50);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (76,44);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (77,50);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (77,45);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (78,50);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (78,45);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (79,50);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (79,45);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (80,50);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (80,45);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (81,50);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (81,45);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (81,30);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (82,17);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (82,42);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (82,34);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (83,35);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (83,54);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (84,17);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (84,59);
+insert into eloado_mufaj (eloado_id,mufaj_id)
+values (84,42);
